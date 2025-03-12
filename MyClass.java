2d7389bc0af4be4add5677f7e30c9be7bf233bf0
SUNNYP <49777105+modsunxx@users.noreply.github.com>
2025-03-12 14:26:35 +0700

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,4 +1,13 @@
 public class MyClass {
+    
+    // main method สำหรับข้อ1
+    // public static void main(String args[]) {
+    //     Skill fb = new Skill("FireBall","Fire",150,7,34);
+    //     System.out.println(fb.getName());
+    //     fb.setAttack((int)(fb.getAttack()*1.5));
+    //     fb.setHit(8);
+    //     System.out.println(fb.toString());
+    // }
     public static void main(String args[]) {
         Skill fb = new Skill("FireBall","Fire",150,7,70);
         Wizard wz = new Wizard(100, fb);
